3967b3ab3869b4e24eeee5543ec165603bfdbe82
devey <yassinesania@gmail.com>
2021-04-09 15:36:14 +0200

    Implement basic functionalities
    - draw with seed
    - createDesign
    - viewMyDesign
    - burnMyDesign

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,74 +1,44 @@
-import { logging, RNG } from'near-sdk-as'
+import { logging, context } from 'near-sdk-as';
+import { generate } from './generate';
+import { Design, designs } from './models';
 
-const SIZE = 28;
-const HALF_SIZE = SIZE / 2;
 
-const ONE = 1;
+export function createDesign(seed: i32) : void {
+    assert(seed >= 0, "Seed needs to be valid.");
+    assert(!designs.contains(context.sender), "You can only own one design.")
 
-let schema = ["🟣", "🟡️️", "⚫️", "⭕️", "🔘"];
-  
-export function draw() : void{ // Array<string> 
-    let output = Array.create<string>(SIZE * SIZE);
-    
-    let a : i32 = <i32>randomNum();
+    let instructions = generate(seed);
 
-    let c : i32 = 0;
-    let x : i32 = 0;
-    let y : i32 = 0;
-    let v : i32 = 0;
-    let value : string = "";
-    let mod = (a % 11) + 5;
+    let design = new Design(instructions);
 
-    for (let i = 0; i < SIZE; i++) {
-        y = (2 * (i - HALF_SIZE) + 1);
-        if (a % 3 == 1) {
-            y = -y;
-        } else if (a % 3 == 2) {
-            y = <i32>abs(y);
-        }
-        y = y * a;
-        for (let j = 0; j < SIZE; j++) {
-            x = (2 * (j - HALF_SIZE) + 1);
-            if (a % 2 == 1) {
-                x = <i32>abs(x);
-            }
-            x = x * a;
-            v = <i32>(abs(x * y / ONE) % mod);
-            if (v < 5) {
-                value = schema[v];
-            } else {
-                value = "⚪️";
-            }
-            output[c] = value + "";
-            c++;
-        }
-        output[c] = "\n";
-        c++;
-    }
+    logging.log(`\n\n\t> ART / Seed: ${seed} \n\n\t` + instructions.replaceAll("\n", "\n\t") + "\n")
 
-    logging.log("Art")
-    logging.log(`\n\n > ART / Seed: ${a} \n\n` + output.join("") + "\n")
-    // return output;
+    logging.log("\n\n\tClaimed Art")
+
+    designs.set(context.sender, design);
 }
 
-function abs(n: i32) : i32 {
-    if (n >= 0) return n;
-    return -n;
+export function viewMyDesign() : void {
+    let design = designs.getSome(context.sender);
+
+    logging.log(`\n\n\t> Your Art \n\n\t` + design.instructions.replaceAll("\n", "\n\t") + "\n")
+
 }
 
+export function burnMyDesign() : void {
+    assert(designs.contains(context.sender), "No design to burn here.");
 
-// function randomNum(): u32 {
-//     let buf = math.randomBuffer(5);
-//     return (
-//       (0xff & buf[0]) << 32 |
-//       (0xff & buf[1]) << 24 |
-//       (0xff & buf[2]) << 16 |
-//       (0xff & buf[3]) << 8  |
-//       (0xff & buf[4]) << 0
-//     ) % 100;
-// }
- 
-function randomNum(): u8 {
-    const rng = new RNG<u8>(1, 10000000)
-    return rng.next()
+    designs.delete(context.sender);
+
+    logging.log(`\n\n\t> Design burned \n\n\t`)
+} 
+
+export function draw(seed : i32 = 0) : void {
+    let instructions = generate(seed);
+
+    logging.log(`\n\n\t> ART \n\n\t` + instructions.replaceAll("\n", "\n\t") + "\n")
 }
+
+// burn/delete user might want to do it to create new one
+
+
