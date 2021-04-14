2edc6e7c65a7333e0d9ab94218efc7c78451df10
devey <yassinesania@gmail.com>
2021-04-14 14:14:23 +0200

    Init Near

---
+++ frontend/config.js
@@ -0,0 +1,17 @@
+const contractName = 'ncd.ys24.testnet';
+
+module.exports = function getConfig(isServer = false) {
+	let config = {
+		networkId: 'testnet',
+		nodeUrl: 'https://rpc.testnet.near.org',
+		walletUrl: 'https://wallet.testnet.near.org',
+		helperUrl: 'https://helper.testnet.near.org',
+		contractName,
+    contractMethods: {
+      changeMethods: [''],
+      viewMethods: [''],
+    }
+	};
+
+	return config;
+};
---
+++ frontend/utils/near-utils.js
@@ -0,0 +1,23 @@
+import getConfig from '../config';
+import * as nearAPI from 'near-api-js';
+
+export const {
+	networkId, nodeUrl, walletUrl, nameSuffix,
+	contractName, contractMethods
+} = getConfig();
+
+const {
+	Contract,
+} = nearAPI;
+
+export function getContract(account) {
+	return new Contract(account, contractName, { ...contractMethods });
+}
+
+export const getWallet = async () => {
+	const near = await nearAPI.connect({
+		networkId, nodeUrl, walletUrl, deps: { keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore() },
+	});
+	const wallet = new nearAPI.WalletAccount(near);
+	return { near, wallet };
+};
