645a87418e466640256a34927b15fa2a14035b0e
devey <yassinesania@gmail.com>
2021-04-19 17:23:49 +0200

    Fix framer-motion components

--- frontend/components/MotionComponents.tsx
+++ frontend/components/MotionComponents.tsx
@@ -3,19 +3,19 @@ import { Box, BoxProps, Grid, GridProps, Button, ButtonProps } from 'theme-ui'
 import Text, { TextProps } from './Text'
 import Flex, { FlexProps } from './Flex'
 
-const MotionBox = motion.custom(Box)
+const MotionBox = motion(Box)
 export type MotionBoxProps = BoxProps & MotionProps
 
-const MotionFlex = motion.custom(Flex)
+const MotionFlex = motion(Flex)
 export type MotionFlexProps = FlexProps & MotionProps
 
-const MotionGrid = motion.custom(Grid)
+const MotionGrid = motion(Grid)
 export type MotionGridProps = GridProps & MotionProps
 
-const MotionButton = motion.custom(Button)
+const MotionButton = motion(Button)
 export type MotionButtonProps = ButtonProps & MotionProps
 
-const MotionText = motion.custom(Text)
+const MotionText = motion(Text)
 export type MotionTextProps = TextProps & MotionProps
 
 export { MotionBox, MotionGrid, MotionFlex, MotionText, MotionButton }
