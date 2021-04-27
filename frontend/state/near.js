3c795d13836bc606421744a93f36b080cc8b107e
devey <yassinesania@gmail.com>
2021-04-27 15:38:58 +0200

    Send to origin on sign in success or error

--- frontend/state/near.js
+++ frontend/state/near.js
@@ -19,7 +19,7 @@ export const initNear = () => async ({ update, getState, dispatch }) => {
 	const { near, wallet } = await getWallet();
 
 	wallet.signIn = () => {
-		wallet.requestSignIn(contractName, 'YS24 Alpha');
+		wallet.requestSignIn(contractName, 'Mojio', window.location.origin, window.location.origin);
 	};
 	const signOut = wallet.signOut;
 	wallet.signOut = () => {
