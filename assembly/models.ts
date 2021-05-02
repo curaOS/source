d9209bd182419444a887ab66236423c974c6f4fd
devey <yassinesania@gmail.com>
2021-05-02 14:17:18 +0200

    Implement nft_metadata function

--- assembly/models.ts
+++ assembly/models.ts
@@ -1,6 +1,17 @@
 import { context, PersistentMap, PersistentSet } from "near-sdk-as";
+import { NFTContractMetadataType } from "./types"
 
 type AccountId = string;
+
+export const NFTContractMetadata : NFTContractMetadataType = {
+    spec: "nft-1.0.0",
+    name: "Share",
+    symbol: "SHARE",
+    icon: null,
+    base_uri: null,
+    reference: null,
+    reference_hash: null,
+}
 
 @nearBindgen
 export class Design {
