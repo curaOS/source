5f366d164853ff152b8263915919200ac0ede5c3
devey <yassinesania@gmail.com>
2021-05-02 14:15:30 +0200

    NFTContractMetadataType

---
+++ assembly/types.ts
@@ -0,0 +1,10 @@
+
+export class NFTContractMetadataType {
+    spec: string;
+    name: string;
+    symbol: string;
+    icon: string | null;
+    base_uri: string | null;
+    reference: string | null;
+    reference_hash: string | null;
+}
