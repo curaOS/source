4dcdea6d98ca70e1d228c3c375d5b0f4825a6bec
devey <yassinesania@gmail.com>
2021-05-04 17:37:55 +0200

    Add TokenMetada
    + move instructions to extra

--- assembly/models.ts
+++ assembly/models.ts
@@ -18,15 +18,43 @@ export class NFTContractMetadata {
 }
 
 @nearBindgen
+class TokenMetadata {
+    constructor(
+        public title: string = "",
+        public issued_at: string = "",
+        public copies: number = 1,
+        public extra: string = "", 
+        public description: string = "",
+        public media: string = "",
+        public media_hash: string = "",
+        public expires_at: string = "",
+        public starts_at: string = "",
+        public updated_at: string = "",
+        public reference: string = "",
+        public reference_hash: string = "",
+    ) {}
+  }
+
+@nearBindgen
 export class Design {
     id: string;
     owner_id: string;
+    metadata: TokenMetadata;
     constructor(
-        public instructions: Array<i32>,
+        instructions: Array<i32>,
         public seed: i32, 
     ) {
         this.id = context.sender;
         this.owner_id = context.sender;
+
+       
+        const title = `${seed}`; 
+        const issued_at = context.blockTimestamp.toString();
+        const copies : number = 1;
+        const extra = `"{'instructions': '${instructions}'}"`;
+
+        this.metadata = new TokenMetadata(title, issued_at, copies, extra);
+
     }
 }
 
