7d209fb4b3531765f8f2b02bdf340390a10c3686
devey <yassinesania@gmail.com>
2021-05-04 14:14:51 +0200

    Syntax

--- assembly/models.ts
+++ assembly/models.ts
@@ -2,11 +2,15 @@ import { context, PersistentMap, PersistentSet } from "near-sdk-as";
 
 type AccountId = string;
 
+const NFT_SPEC = "nft-1.0.0";
+const NFT_NAME = "Share";
+const NFT_SYMBOL = "SHARE";
+
 @nearBindgen
 export class NFTContractMetadata {
-    spec: string = "nft-1.0.0";
-    name: string = "Share";
-    symbol: string = "SHARE";
+    spec: string = NFT_SPEC;
+    name: string = NFT_NAME;
+    symbol: string = NFT_SYMBOL;
     icon: string;
     base_uri: string;
     reference: string;
