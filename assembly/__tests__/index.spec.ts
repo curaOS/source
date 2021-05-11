ef121b7afeeb3498045fd8a464a75c18a3c391dc
devey <yassinesania@gmail.com>
2021-05-11 09:43:38 +0200

    Add royalty design tests

--- assembly/__tests__/index.spec.ts
+++ assembly/__tests__/index.spec.ts
@@ -1,22 +1,17 @@
 import { VM, VMContext } from "near-mock-vm";
-import { env } from 'near-sdk-as'
-import { design, nft_metadata } from "../index";
-import { generate } from '../generate';
-import { Royalty, Design, stakers, SHARE_PRICE, DESIGN_PRICE } from '../models';
+import { logging, util } from 'near-sdk-as'
+import { design, nft_metadata, claimMyDesign } from "../index";
+import { Royalty, Design, stakers, SHARE_PRICE, DESIGN_PRICE, Extra, designs } from '../models';
 
 const SEED_EXAMPLE = 100;
 const condo = "condo"
 
-describe("design", () => {
+describe("- DESIGN -", () => {
   beforeEach(() => {
     VMContext.setSigner_account_id(condo)
   })
-
-  afterEach(() => {
-    // cleanup
-  })
   
-  it("creates new", () => {
+  it("xxx creates new", () => {
     const newDesign = design()
 
     expect(newDesign.owner_id).toBe(condo)
@@ -25,13 +20,32 @@ describe("design", () => {
 })
 
 
-describe("contract", () => {
-  it("returns metadata", () => {
+describe("- CONTRACT -", () => {
+  it("xxx returns metadata", () => {
     const contractMetadata = nft_metadata();
 
 
     expect(contractMetadata.spec).toBe("nft-1.0.0");
     expect(contractMetadata.name).toBe("Share");
     expect(contractMetadata.symbol).toBe("SHARE");
+  })
+});
+
+describe("- ROYALTY -", () => {
+  beforeEach(() => {
+    VMContext.setSigner_account_id(condo)
+  })
+  
+
+  it("xxx creates Design object with Royalty type design", () => {
+    const newDesign  = design();
+
+    expect(newDesign).not.toBeNull();
+    expect(newDesign.metadata).not.toBeNull();
+    expect(newDesign.royalty).not.toBeNull();
+
+    const metadataExtra = util.parseFromBytes<Extra>(newDesign.metadata.extra);
+
+    expect(metadataExtra.value).toBe(DESIGN_PRICE);
   })
 });
