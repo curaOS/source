518b126e2b202d06bd2e3cc271abe46fd5fb2311
devey <yassinesania@gmail.com>
2021-05-12 11:41:05 +0200

    Update ft_balance_of

--- contracts/YSN/assembly/index.ts
+++ contracts/YSN/assembly/index.ts
@@ -15,9 +15,11 @@ export function ft_total_supply(): string {
 export function ft_balance_of(
     account_id: string
 ): string {
-  const balance = balances.get(account_id, null);
-
-  return (balance == null) ? "0" : balance.toString();
+  if (balances.contains(account_id)) {
+    return balances.getSome(account_id).toString();
+  } else {
+    return "0";
+  }
 }
 
 export function ft_metadata(): FTContractMetadata {
