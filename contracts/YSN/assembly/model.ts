b51e4cf74434b7263fb3735d2719286ce7a9a282
devey <yassinesania@gmail.com>
2021-05-19 13:55:38 +0200

    Clean & update FT_NAME

--- contracts/YSN/assembly/model.ts
+++ contracts/YSN/assembly/model.ts
@@ -1,11 +1,10 @@
 import { PersistentMap, u128 } from "near-sdk-as";
 
 const FT_SPEC = "ft-1.0.0";
-const FT_NAME = "Yassine";
+const FT_NAME = "Ysn";
 const FT_SYMBOL = "YSN";
 const FT_DECIMALS : u8 = 24;
 
-export const INITIAL_SUPPLY : u128 = u128.Zero;
 
 @nearBindgen
 export class FTContractMetadata {
