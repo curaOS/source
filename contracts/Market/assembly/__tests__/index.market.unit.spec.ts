3256ddf010df1abeea8c022365f8aab1373fc548
devey <yassinesania@gmail.com>
2021-05-27 15:58:34 +0200

    Init test

---
+++ contracts/Market/assembly/__tests__/index.market.unit.spec.ts
@@ -0,0 +1,17 @@
+import { storage, u128 } from "near-sdk-as";
+import { VMContext, VM } from "near-mock-vm";
+import { init, MEDIA_CONTRACT_KEY } from '../index';
+
+const condo = "condo"
+
+describe("- CONTRACT -", () => {
+  beforeEach(() => {
+    VMContext.setSigner_account_id(condo)
+  })
+
+  it("xxx inits", () => {
+    init("media.contract");
+
+    expect(storage.get<string>(MEDIA_CONTRACT_KEY)).toBe("media.contract");
+  })
+});
