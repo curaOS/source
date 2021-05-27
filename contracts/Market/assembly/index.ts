14d5d177f3516d2def30326d658a276af640789d
devey <yassinesania@gmail.com>
2021-05-27 15:51:18 +0200

    Move market to own contract folder

---
+++ contracts/Market/assembly/index.ts
@@ -0,0 +1,17 @@
+import { storage } from 'near-sdk-as';
+import { AccountId } from "./models";
+
+/** 
+ * Init
+ */
+
+// The media contract that can call this market
+const MEDIA_CONTRACT_KEY = "media_contract";
+
+export function init(media_contract: AccountId): void {
+    assert(storage.get<string>("init") == null, "Already initialized");
+  
+    storage.set(MEDIA_CONTRACT_KEY, media_contract);
+  
+    storage.set("init", "done");
+  }
