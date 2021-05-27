f4f8fea17ae7633c5e7612e9086053c6294df3d7
devey <yassinesania@gmail.com>
2021-05-27 19:27:47 +0200

    Market set_bid_shares

--- contracts/Market/assembly/index.ts
+++ contracts/Market/assembly/index.ts
@@ -1,5 +1,5 @@
 import { storage } from 'near-sdk-as';
-import { AccountId } from "./models";
+import { AccountId, bid_shares, BidShares } from "./models";
 
 /** 
  * Init
@@ -7,6 +7,18 @@ import { AccountId } from "./models";
 
 // The media contract that can call this market
 export const MEDIA_CONTRACT_KEY = "media_contract";
+
+export function set_bid_shares(
+    token_id: string,
+    prev_owner: u16,
+    creator: u16,
+    owner: u16,
+): void {
+    const new_bid_shares = new BidShares(prev_owner, creator, owner);
+    bid_shares.set(token_id, new_bid_shares);
+
+}
+
 
 export function init(media_contract: AccountId): void {
     assert(storage.get<string>("init") == null, "Already initialized");
