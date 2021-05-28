b5b62fbb81da411fc7a4cc8c5e37318d808291bc
devey <yassinesania@gmail.com>
2021-05-28 15:49:40 +0200

    Market implement set bid

--- contracts/Market/assembly/models.ts
+++ contracts/Market/assembly/models.ts
@@ -1,8 +1,8 @@
 import { context, PersistentMap, u128 } from "near-sdk-as";
 
 export type AccountId = string;
-type CurrencyId = string;
-type TokenId = string;
+export type CurrencyId = string;
+export type TokenId = string;
 
 type Bids = Map<AccountId, Bid>;
 
@@ -18,12 +18,13 @@ const ZERO_PERCENT : u32 = 0; // 0%
 
 @nearBindgen
 export class Bid {
-    amount: u128;
-    currency: CurrencyId = "near";
-    bidder : AccountId;
-    recipient : AccountId;
-    sell_on_share : u32;
-    constructor() { }
+    constructor(
+        public amount: u128,
+        public bidder : AccountId,
+        public recipient : AccountId,
+        public sell_on_share : u32,
+        public currency: CurrencyId = "near",
+    ) { }
 }
 
 @nearBindgen
