4178abc39291ada042f96349392a18664ab82818
devey <yassinesania@gmail.com>
2021-06-02 15:27:58 +0200

    Prettify

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -90,43 +90,41 @@ const SIZE = 32
 
 const CANVAS_WIDTH = [300, 400, 500, 600, 700] // 0-4
 
-const design =
-    (instructions = [], mediaIndex = 0) =>
-    (p) => {
-        let c = ''
-        const designDimension = CANVAS_WIDTH[mediaIndex]
+const design = (instructions = [], mediaIndex = 0) => (p) => {
+    let c = ''
+    const designDimension = CANVAS_WIDTH[mediaIndex]
 
-        const canvasStep = [
-            designDimension / (SIZE * 1.1),
-            designDimension / (SIZE * 1.1),
-        ]
-        const canvasTextSize = designDimension / (SIZE * 1.7)
+    const canvasStep = [
+        designDimension / (SIZE * 1.1),
+        designDimension / (SIZE * 1.1),
+    ]
+    const canvasTextSize = designDimension / (SIZE * 1.7)
 
-        const canvasStart = [
-            (designDimension - canvasStep[0] * (SIZE - 0.2)) / 2,
-            (designDimension - canvasStep[1] * (SIZE - 1.5)) / 2,
-        ]
+    const canvasStart = [
+        (designDimension - canvasStep[0] * (SIZE - 0.2)) / 2,
+        (designDimension - canvasStep[1] * (SIZE - 1.5)) / 2,
+    ]
 
-        p.setup = () => {
-            p.createCanvas(designDimension, designDimension)
-            p.noLoop()
-            if (instructions.length > 0) {
-                p.background(0)
-                p.textSize(canvasTextSize)
-                for (let i = 0; i < SIZE; i++) {
-                    for (let j = 0; j < SIZE; j++) {
-                        c = String.fromCodePoint(instructions[j + i * SIZE])
+    p.setup = () => {
+        p.createCanvas(designDimension, designDimension)
+        p.noLoop()
+        if (instructions.length > 0) {
+            p.background(0)
+            p.textSize(canvasTextSize)
+            for (let i = 0; i < SIZE; i++) {
+                for (let j = 0; j < SIZE; j++) {
+                    c = String.fromCodePoint(instructions[j + i * SIZE])
 
-                        p.text(
-                            c,
-                            canvasStart[0] + j * canvasStep[0],
-                            canvasStart[1] + i * canvasStep[1]
-                        )
-                    }
+                    p.text(
+                        c,
+                        canvasStart[0] + j * canvasStep[0],
+                        canvasStart[1] + i * canvasStep[1]
+                    )
                 }
             }
         }
     }
+}
 
 const Design = ({ instructions }: { instructions: Array<number> }) => {
     const mediaIndex = useBreakpointIndex()
