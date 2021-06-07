25077af5d519171a02701f5b7c1f21f25931ae0c
devey <yassinesania@gmail.com>
2021-06-07 11:01:52 +0200

    Next and Contracts accont files

---
+++ contracts/accounts.ts
@@ -0,0 +1,3 @@
+export const YSN_ADDRESS : string = "ysn-1_0_0.ysn.testnet"
+export const SHARE_ADDRESS : string = "share.ysn-1_0_0.ysn.testnet"
+export const SHARE_MARKET_ADDRESS : string = "market.share.ysn-1_0_0.ysn.testnet"
---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        YSN_ADDRESS: 'ysn-1_0_0.ysn.testnet',
+        SHARE_ADDRESS: 'share.ysn-1_0_0.ysn.testnet',
+        SHARE_MARKET_ADDRESS: 'market.share.ysn-1_0_0.ysn.testnet',
+    },
+}
