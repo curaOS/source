50abdf58af43553cad4b97af621a019e4c347976
devey <yassinesania@gmail.com>
2021-06-21 14:24:21 +0200

    getContractMethods

--- frontend/utils/near-utils.js
+++ frontend/utils/near-utils.js
@@ -1,6 +1,10 @@
 import getConfig from '../config'
 import * as nearAPI from 'near-api-js'
 
+const ftContract = process.env.YSN_ADDRESS
+const nftContract = process.env.SHARE_ADDRESS
+const marketContract = process.env.SHARE_MARKET_ADDRESS
+
 export const {
     networkId,
     nodeUrl,
@@ -25,4 +29,33 @@ export const getWallet = async () => {
     })
     const wallet = new nearAPI.WalletAccount(near)
     return { near, wallet }
+}
+
+export function getContractMethods(contractName) {
+    switch (contractName) {
+        case ftContract:
+            return {
+                changeMethods: [],
+                viewMethods: ['ft_balance_of'],
+            }
+        case nftContract:
+            return {
+                changeMethods: [
+                    'set_bid',
+                    'remove_bid',
+                    'accept_bid',
+                    'design',
+                    'claim_media',
+                    'burn_design',
+                    'view_media',
+                    'nft_tokens',
+                ],
+                viewMethods: ['nft_total_supply'],
+            }
+        case marketContract:
+            return {
+                changeMethods: [],
+                viewMethods: ['get_bids', 'get_bidders_bids'],
+            }
+    }
 }
