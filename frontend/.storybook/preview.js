cc999af9b4b858482bec7ce8651ebe314442412c
devey <yassinesania@gmail.com>
2021-06-28 13:29:10 +0200

    Add IBM font and theme-ui provider to storybook decorator

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,31 @@
+import React from 'react'
+import { ThemeProvider } from 'theme-ui'
+import { theme } from '../theme'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
+
+export const decorators = [
+    (Story) => (
+        <ThemeProvider theme={theme}>
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
+            />
+            <div
+                sx={{
+                    backgroundColor: 'gray.2',
+                }}
+            >
+                <Story />
+            </div>
+        </ThemeProvider>
+    ),
+]
