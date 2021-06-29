0132a7ee3d23cfb1dfae838642701f409b4034ef
devey <yassinesania@gmail.com>
2021-06-29 13:25:11 +0200

    Add title to Header and stories

--- frontend/components/Header.stories.tsx
+++ frontend/components/Header.stories.tsx
@@ -12,8 +12,11 @@ const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />
 
 export const Connected = Template.bind({})
 Connected.args = {
+    title: 'SHARE',
     accountId: 'yassine.testnet',
 }
 
 export const Disconnected = Template.bind({})
-Disconnected.args = {}
+Disconnected.args = {
+    title: 'SHARE',
+}
