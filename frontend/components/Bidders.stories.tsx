b4ad50117bf7ff0b3423a22dda068025d69546e6
devey <yassinesania@gmail.com>
2021-07-05 12:38:57 +0200

    Bidders stories

---
+++ frontend/components/Bidders.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Bidders from './Bidders'
+
+export default {
+    title: 'Elements/Bidders',
+    component: Bidders,
+} as ComponentMeta<typeof Bidders>
+
+const Template: ComponentStory<typeof Bidders> = (args) => <Bidders {...args} />
+
+export const Zero = Template.bind({})
+Zero.args = {}
+
+export const Tre = Template.bind({})
+Tre.args = {
+    bidders: {
+        'frank.testnet': {
+            amount: '3300000000000000000000000',
+            bidder: 'prova.testnet',
+            recipient: 'ysn.testnet',
+            sell_on_share: 2000,
+        },
+        'kanye.testnet': {
+            amount: '93800000000000000000000000',
+            bidder: 'prova.testnet',
+            recipient: 'ysn.testnet',
+            sell_on_share: 200,
+        },
+        'tyler.testnet': {
+            amount: '52300000000000000000000000',
+            bidder: 'tyler.testnet',
+            recipient: 'ysn.testnet',
+            sell_on_share: 0,
+        },
+    },
+}
