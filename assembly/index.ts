dff45af99669492fdd2e0c122af9fd43602e0eda
Sherif Abushadi <sherif@amgando.org>
2021-07-07 21:50:38 +0300

    2. refactor calls to Context.sender to save on gas 3. refactor logging messages into a single logMessage function 4. refactor to add guard clause before calling designs.getSome() 5. refactor Design class by adding a static generate function 6. refactor two Persistent* classes into one 7. surfaced pagination params to viewDesigns contract method

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,57 +1,58 @@
-import { logging, context } from 'near-sdk-as';
+import { logging, Context } from 'near-sdk-as';
 import { generate } from './generate';
-import { Design, designs, owners } from './models';
+import { Design, designs } from './models';
 
 
-export function claimMyDesign(seed: i32) : void {
+export function claimMyDesign(seed: i32): void {
     assert(seed >= 0, "Seed needs to be valid.");
-    assert(!designs.contains(context.sender), "You can only own one design.")
+    const sender = Context.sender
+    assert(!designs.contains(sender), "You can only own one design.")
 
-    let instructions = generate(seed);
+    let design = Design.generate(seed);
 
-    let design = new Design(instructions);
-
-    logging.log(`\n\n\t> ART / Seed: ${seed} \n\n\t` + instructions.replaceAll("\n", "\n\t") + "\n")
+    logMessage(`ART / Seed: ${seed}`, design.instructions)
 
     logging.log("\n\n\tClaimed Art")
 
-    designs.set(context.sender, design);
-    owners.add(context.sender);
+    designs.set(sender, design);
 }
 
-export function viewMyDesign() : void {
-    let design = designs.getSome(context.sender);
+export function viewMyDesign(): void {
+    const sender = Context.sender
+    assert(designs.contains(sender), `[Account ${sender}] has no registered designs.`)
+    let design = designs.getSome(sender);
 
-    logging.log(`\n\n\t> Your Art \n\n\t` + design.instructions.replaceAll("\n", "\n\t") + "\n")
-
+    logMessage("Your Art", design.instructions)
 }
 
-export function burnMyDesign() : void {
-    assert(designs.contains(context.sender), "No design to burn here.");
+export function burnMyDesign(): void {
+    const sender = Context.sender
+    assert(designs.contains(sender), "No design to burn here.");
 
-    designs.delete(context.sender);
-    owners.delete(context.sender);
+    designs.delete(sender);
 
     logging.log(`\n\n\t> Design burned \n\n\t`)
-} 
-
-export function design(seed : i32 = 0) : void {
-    let instructions = generate(seed);
-
-    logging.log(`\n\n\t> ART \n\n\t` + instructions.replaceAll("\n", "\n\t") + "\n")
 }
 
-export function viewDesigns() : void {
-    const ownersValues = owners.values();
-    let design : Design;
-    
-    for (let i = 0; i < owners.size; i++) {
-        design = designs.getSome(ownersValues[i]);
-        logging.log(`\n\n\t> Owner : ${design.owner} \n\n\t` + design.instructions.replaceAll("\n", "\n\t") + "\n")
+export function design(seed: i32 = 0): void {
+    let instructions = generate(seed);
+
+    logMessage("ART", instructions)
+}
+
+export function viewDesigns(start: i32 = 0, end: i32 = design.length): void {
+    const ownersValues = designs.values(start, end)
+    let design: Design;
+
+    for (let i = 0; i < ownersValues.length; i++) {
+        design = ownersValues[i];
+        logMessage(`Owner : ${design.owner}`, design.instructions)
     }
+}
+
+function logMessage(prefix: string, instructions: string): void {
+    logging.log(`\n\n\t> ${prefix} \n\n\t${instructions.replaceAll("\n", "\n\t")}\n`)
 }
 
 
 // burn/delete user might want to do it to create new one
-
-
--- assembly/models.ts
+++ assembly/models.ts
@@ -1,17 +1,18 @@
-import { context, PersistentMap, PersistentSet } from "near-sdk-as";
+import { Context, PersistentUnorderedMap } from "near-sdk-as";
+import { generate } from './generate';
 
 type AccountId = string;
 
 @nearBindgen
 export class Design {
-    owner: string;
+    owner: string = Context.sender;
     constructor(
         public instructions: string
-    ) {
-        this.owner = context.sender;
+    ) { }
+
+    static generate(seed: i32): Design {
+        return new Design(generate(seed))
     }
 }
 
-export const designs = new PersistentMap<AccountId, Design>("d");
-
-export const owners = new PersistentSet<AccountId>("o")
+export const designs = new PersistentUnorderedMap<AccountId, Design>("d");
