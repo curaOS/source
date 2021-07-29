6d79bdea0c83de5a8376bb9e271c8b35e05837f7
devey <yassinesania@gmail.com>
2021-07-29 11:17:54 +0200

    Clean

--- frontend/config.js
+++ frontend/config.js
@@ -1,26 +1,9 @@
-const contractName = process.env.SHARE_ADDRESS
-
 module.exports = function getConfig(isServer = false) {
     let config = {
         networkId: 'testnet',
         nodeUrl: 'https://rpc.testnet.near.org',
         walletUrl: 'https://wallet.testnet.near.org',
         helperUrl: 'https://helper.testnet.near.org',
-        contractName,
-        contractMethods: {
-            changeMethods: [
-                'set_bid',
-                'remove_bid',
-                'accept_bid',
-                'design',
-                'claim_media',
-                'burn_design',
-                'view_media',
-                'nft_tokens',
-                'nft_tokens_for_owner',
-            ],
-            viewMethods: ['nft_total_supply'],
-        },
     }
 
     return config
