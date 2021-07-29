a533a4a6994a55d69d21ecb89b85dd31b3e19dc1
devey <yassinesania@gmail.com>
2021-07-29 11:33:18 +0200

    Hardcoded contract

--- contracts/Market/assembly/xcc_media.ts
+++ contracts/Market/assembly/xcc_media.ts
@@ -3,7 +3,7 @@ import { SHARE_ADDRESS } from '../../accounts'
 
 const XCC_MEDIA_NFT_TRANSFER_GAS = 125000000000000
 
-const MEDIA_CONTRACT = SHARE_ADDRESS
+const MEDIA_CONTRACT = 'ml1c.ysn-1_0_0.ysn.testnet'
 
 /**
  * media.nft_transfer
