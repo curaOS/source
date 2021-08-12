4726411d4a6bac57465566fa74c7d09c6f5a1661
devey <yassinesania@gmail.com>
2021-08-12 18:44:59 +0200

    Add srcDoc rendering to RenderIframe

--- frontend/components/RenderIframe.tsx
+++ frontend/components/RenderIframe.tsx
@@ -6,21 +6,38 @@ const CANVAS_WIDTH = [300, 400, 500, 600, 700] // 0-4
 
 export default function RenderIframe({
     mediaURI,
+    code,
     small,
 }: {
-    mediaURI: string
+    mediaURI?: string
+    code?: string
     small: boolean
 }) {
     const mediaIndex = useBreakpointIndex()
 
     const designDimension = CANVAS_WIDTH[mediaIndex] / (small ? 2 : 1)
 
-    return (
-        <iframe
-            src={mediaURI}
-            width={designDimension}
-            height={designDimension}
-            frameBorder="0"
-        ></iframe>
-    )
+    if (code) {
+        return (
+            <iframe
+                srcDoc={code}
+                width={designDimension}
+                height={designDimension}
+                frameBorder="0"
+            ></iframe>
+        )
+    }
+
+    if (mediaURI) {
+        return (
+            <iframe
+                src={mediaURI}
+                width={designDimension}
+                height={designDimension}
+                frameBorder="0"
+            ></iframe>
+        )
+    }
+
+    return null
 }
