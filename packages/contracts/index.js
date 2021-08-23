f0757184f31d26589ccc74c59dcf67d7c38d049e
devey <yassinesania@gmail.com>
2021-08-23 18:15:02 +0200

    Build contracts command

---
+++ packages/contracts/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const { exec } = require('child_process')
+const ora = require('ora')
+const async = require('async')
+const program = require('commander')
+
+const log = console.log
+
+program
+    .description('Build contracts.')
+    .option('--contract <name>', 'Contract to build')
+    .action(function (options) {
+        const commandToRun = `asb -d ${__dirname}/as/${options.contract}`
+
+        const spinner = ora(`Build ${options.contract}`).start()
+        async.map([commandToRun], exec, function (err, results) {
+            err ? log(`${chalk.red(err.message)}`) : results.map((r) => log(r))
+            !err ? spinner.succeed('Success') : spinner.fail('Failure')
+        })
+    })
+
+program.parse(process.argv)
