560ecdd8b511d044295e351b36130c7604396c72
devey <yassinesania@gmail.com>
2021-08-24 15:34:38 +0200

    Move Bidders to packages components

---
+++ packages/components/src/Bidders.tsx
@@ -0,0 +1,81 @@
+/** @jsxImportSource theme-ui */
+import { Box, Heading, Text, Button } from 'theme-ui'
+import { utils } from 'near-api-js'
+
+export function Bidders({
+    bidders = {},
+    onAcceptBid,
+}: {
+    bidders: {
+        [key: string]: {
+            amount: string
+            bidder: string
+            recipient: string
+            sell_on_share: number
+            currency: string
+        }
+    }
+    onAcceptBid: (bidder: string) => void
+}) {
+    return (
+        <Box
+            sx={{
+                width: 300,
+                pb: 12,
+                pt: 1,
+                px: 20,
+                borderWidth: 2,
+                borderStyle: 'solid',
+                borderColor: 'muted',
+                borderRadius: 4,
+            }}
+        >
+            <Text sx={{ color: 'secondary', fontSize: 12 }}>BIDS</Text>
+            {!Object.entries(bidders).length && (
+                <div>
+                    <Heading as="h5" sx={{ color: 'text' }}>
+                        -
+                    </Heading>
+                </div>
+            )}
+            {Object.entries(bidders).map(([bidder, bid]) => {
+                return (
+                    <div
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            mt: 2,
+                        }}
+                    >
+                        <div>
+                            <Heading as="h5" sx={{ color: 'text' }}>
+                                {bid.bidder}
+                            </Heading>
+                        </div>
+                        <div
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                textAlign: 'right',
+                            }}
+                        >
+                            <Heading as="h5" sx={{ color: 'text' }}>
+                                {utils.format.formatNearAmount(bid.amount, 5)}Ⓝ
+                            </Heading>
+                            <Text sx={{ fontSize: 12 }}>
+                                Resale Fee <b>{bid.sell_on_share / 100}%</b>
+                            </Text>
+                            <Button
+                                sx={{ pt: 0, pb: 0, fontSize: 12 }}
+                                onClick={() => onAcceptBid(bidder)}
+                            >
+                                Accept
+                            </Button>
+                        </div>
+                    </div>
+                )
+            })}
+        </Box>
+    )
+}
