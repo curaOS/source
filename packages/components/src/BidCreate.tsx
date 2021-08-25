003482b12c2e7089c52b69ff47a9a8cc4f36c823
devey <yassinesania@gmail.com>
2021-08-25 14:30:55 +0200

    Move unwired components from examples

---
+++ packages/components/src/BidCreate.tsx
@@ -0,0 +1,65 @@
+// @ts-nocheck
+/** @jsxImportSource theme-ui */
+
+import React, { useState } from 'react'
+import { Box, Heading, Select, Label, Input, Button } from 'theme-ui'
+
+export default function BidCreate({
+    onBid,
+}: {
+    onBid: (amount: string, resale: string) => void
+}) {
+    const [amount, setAmount] = useState(null)
+    const [resale, setResale] = useState('0')
+
+    return (
+        <Box
+            sx={{
+                width: 300,
+                px: 20,
+            }}
+            as="form"
+            onSubmit={(e) => {
+                e.preventDefault()
+                onBid(amount, resale)
+            }}
+        >
+            <Label htmlFor="currency" sx={{ mb: 1 }}>
+                Currency
+            </Label>
+            <Select name="currency" id="currency" mb={1}>
+                <option>NEAR</option>
+            </Select>
+            <Label htmlFor="amount" sx={{ mb: 1 }}>
+                Amount
+            </Label>
+            <Input
+                type="number"
+                step="0.01"
+                name="amount"
+                id="amount"
+                mb={3}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+            />
+            <Label htmlFor="amount" sx={{ mb: 1 }}>
+                Resale fee
+            </Label>
+            <Input
+                type="number"
+                step="1"
+                name="resale"
+                id="resale"
+                mb={3}
+                value={resale}
+                onChange={(e) => setResale(e.target.value)}
+            />
+            <Button sx={{ width: '100%' }} disabled={!amount} variant="uno">
+                <b>
+                    {amount ? `${amount} Ⓝ` : 'Bid'}{' '}
+                    {amount ? `+ ${resale}%` : ''}
+                </b>
+            </Button>
+        </Box>
+    )
+}
---
+++ packages/components/src/Footer.tsx
@@ -0,0 +1,59 @@
+// @ts-nocheck
+/** @jsxImportSource theme-ui */
+
+import React from 'react'
+import Image from 'next/image'
+
+export default function Footer({}: {}) {
+    return (
+        <div
+            sx={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `1rem 2rem`,
+            }}
+        >
+            <div
+                sx={{
+                    margin: `1rem auto`,
+                    background: `black`,
+                    maxWidth: 960,
+                    padding: `0.02rem 0rem`,
+                }}
+            />
+            <footer
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                }}
+            >
+                <div
+                    sx={{
+                        marginTop: 1,
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <a
+                        sx={{
+                            ':hover': {
+                                opacity: '0.5',
+                            },
+                        }}
+                        href="https://twitter.com/Yassine_2024"
+                    >
+                        <Image
+                            src="/twitter-logo-black.png"
+                            alt="Black logo"
+                            width={30}
+                            height={25}
+                        />
+                    </a>
+                </div>
+            </footer>
+        </div>
+    )
+}
