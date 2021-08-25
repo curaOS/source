5eff4e71c8657fb22bb134783902c77e72b31188
devey <yassinesania@gmail.com>
2021-08-25 13:50:10 +0200

    Copy over .storybook from example

---
+++ packages/components/.storybook/main.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const resolve = (p) => path.join(process.cwd(), p)
+
+module.exports = {
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    webpackFinal(config) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            // '@emotion/core': resolve('node_modules/@emotion/react'),
+            // '@emotion/styled': resolve('node_modules/@emotion/styled'),
+            // 'emotion-theming': resolve('node_modules/@emotion/react'),
+        }
+        return config
+    },
+}
