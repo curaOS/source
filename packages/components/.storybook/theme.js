fab2edc7c4253a126565f5c1aa07a993c3e6bef0
devey <yassinesania@gmail.com>
2021-08-25 13:50:10 +0200

    Copy over .storybook from example

---
+++ packages/components/.storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming'
+
+import brandImage from './brand.png'
+
+export default create({
+    base: 'light',
+    brandTitle: 'YSN',
+    brandUrl: 'https://ysn.design/',
+    brandImage: brandImage,
+})
