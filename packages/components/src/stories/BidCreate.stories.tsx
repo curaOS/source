539ac501fa4b5357c4f732a0996a6a6e29defcdf
devey <yassinesania@gmail.com>
2021-08-25 14:31:26 +0200

    Move BidCreate story

---
+++ packages/components/src/stories/BidCreate.stories.tsx
@@ -0,0 +1,17 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react'
+import BidCreate from '../BidCreate'
+
+export default {
+    title: 'Components/Bid Create',
+    component: BidCreate,
+    parameters: {
+        layout: 'centered',
+    },
+} as ComponentMeta<typeof BidCreate>
+
+const Template: ComponentStory<typeof BidCreate> = (args) => (
+    <BidCreate {...args} />
+)
+
+export const Zero = Template.bind({})
+Zero.args = {}
