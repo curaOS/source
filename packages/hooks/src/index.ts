cdcad193b1054b0b920f2b47b5ff23d2a5d63b16
devey <yassinesania@gmail.com>
2021-09-21 16:54:24 +0200

    Cura package and tsconfig
    + Index for hooks

---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,9 @@
+import useNFTContract, { useNFTMethod } from './useNFTContract'
+import { NearHooksProvider, useNearHooksContainer } from './near'
+
+export {
+    useNFTContract,
+    useNFTMethod,
+    NearHooksProvider,
+    useNearHooksContainer,
+}
