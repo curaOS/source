d57d5b8e22bde30040b4de03e0e9e9e9f1562dc4
devey <yassinesania@gmail.com>
2021-09-21 16:44:36 +0200

    Move useNFTContract to hooks

---
+++ packages/hooks/src/useNFTContract.ts
@@ -0,0 +1,59 @@
+// @ts-nocheck
+import { useEffect, useState } from 'react'
+import { useNearHooksContainer } from './near'
+import { getContractMethods } from './near-utils'
+import useSWR from 'swr'
+
+export default function useNFTContract(
+    contractAddress: string = '0.share-nft.testnet'
+) {
+    const [contract, setContract] = useState({ account: null })
+    const { getContract, accountId } = useNearHooksContainer()
+
+    const setupContract = () => {
+        if (contractAddress.includes('undefined')) {
+            return
+        }
+
+        const newContract = getContract(
+            contractAddress,
+            getContractMethods('nft')
+        )
+
+        setContract(newContract)
+    }
+
+    // If contractName or accountId changes a new contract is setup
+    useEffect(setupContract, [accountId, contractAddress])
+
+    return { contract }
+}
+
+export const useNFTMethod = (
+    contractAddress: string,
+    methodName: string,
+    params: {},
+    gas: number
+) => {
+    const { contract } = useNFTContract(contractAddress)
+
+    const validParams = contract?.account?.accountId
+
+    const fetcher = (methodName: string, serializedParams: string) => {
+        const params = JSON.parse(serializedParams)
+        return contract[methodName]({ ...params }, gas).then((res) => {
+            return res
+        })
+    }
+
+    const { data, error } = useSWR(
+        validParams ? [methodName, JSON.stringify(params)] : null,
+        fetcher
+    )
+
+    return {
+        data: data,
+        loading: !error && !data,
+        error: error,
+    }
+}
