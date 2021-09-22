f30fb83dc9eb0519d1405384cff14d51031d8c7f
devey <yassinesania@gmail.com>
2021-09-22 16:54:58 +0200

    Clean near-utils in the example

--- examples/frontend/utils/near-utils.js
+++ examples/frontend/utils/near-utils.js
@@ -1,9 +1,5 @@
 import getConfig from '../config'
 
-const ftContract = process.env.YSN_ADDRESS
-const nftContract = process.env.SHARE_ADDRESS
-const marketContract = process.env.SHARE_MARKET_ADDRESS
-
 export const {
     networkId,
     nodeUrl,
@@ -12,41 +8,3 @@ export const {
     contractName,
     contractMethods,
 } = getConfig()
-
-export function getContractMethods(contractName) {
-    switch (contractName) {
-        case ftContract:
-            return {
-                changeMethods: [],
-                viewMethods: [
-                    'ft_balance_of',
-                    'get_treasury',
-                    'ft_total_supply',
-                ],
-            }
-        case 'nft':
-        case nftContract:
-            return {
-                changeMethods: [
-                    'set_bid',
-                    'remove_bid',
-                    'accept_bid',
-                    'generate',
-                    'claim_media',
-                    'burn_design',
-                    'view_media',
-                    'nft_metadata',
-                    'nft_token',
-                    'nft_tokens',
-                    'nft_tokens_for_owner',
-                ],
-                viewMethods: ['nft_total_supply'],
-            }
-        case 'market':
-        case marketContract:
-            return {
-                changeMethods: [],
-                viewMethods: ['get_bids', 'get_bidders_bids'],
-            }
-    }
-}
