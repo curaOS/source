ce699a146d0137bc780837810254d81da072b094
devey <yassinesania@gmail.com>
2021-09-22 16:23:25 +0200

    Move useMarketContract
    + remove alert and loading from hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,9 +1,12 @@
 import useNFTContract, { useNFTMethod } from './useNFTContract'
+import useMarketContract, { useMarketMethod } from './useMarketContract'
 import { NearHooksProvider, useNearHooksContainer } from './near'
 
 export {
     useNFTContract,
     useNFTMethod,
+    useMarketContract,
+    useMarketMethod,
     NearHooksProvider,
     useNearHooksContainer,
 }
