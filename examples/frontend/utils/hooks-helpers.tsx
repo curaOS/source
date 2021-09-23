7781a126255b052aed03efe067c2592084677b09
devey <yassinesania@gmail.com>
2021-09-23 16:16:55 +0200

    useStatusUpdate helper

---
+++ examples/frontend/utils/hooks-helpers.tsx
@@ -0,0 +1,24 @@
+import { alertMessageState, indexLoaderState } from '../state/recoil'
+import { useSetRecoilState } from 'recoil'
+
+export function useStatusUpdate() {
+    const setAlertMessage = useSetRecoilState(alertMessageState)
+    const setIndexLoader = useSetRecoilState(indexLoaderState)
+
+    let updateStatus = (error, data, validParams) => {
+        if (!error && !data && validParams) {
+            setIndexLoader(true)
+        }
+
+        if (error) {
+            setAlertMessage(error.toString())
+            setIndexLoader(false)
+        }
+
+        if (data) {
+            setIndexLoader(false)
+        }
+    }
+
+    return { updateStatus }
+}
