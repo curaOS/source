1c921013ccbfddff1b19477ca4552b2a1e4045b4
achraf elghinoussi <afaithraf@gmail.com>
2021-10-20 17:15:34 +0100

    Projects slides are now animated + draggable

--- examples/frontend/pages/index.tsx
+++ examples/frontend/pages/index.tsx
@@ -2,7 +2,7 @@
 /** @jsxImportSource theme-ui */
 
 import { IconButton } from 'theme-ui'
-import { alpha } from '@theme-ui/color'
+import { motion, AnimatePresence } from 'framer-motion'
 import { ProjectCard } from '@cura/components'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
@@ -45,42 +45,115 @@ const slideDetails = [
     },
 ]
 
-const Index = () => {
-    const [slide, setSlide] = useState(0)
-    const router = useRouter()
+const slideVariants = {
+    enter: (direction: number) => {
+        return {
+            y: '-50%',
+            x: direction > 0 ? 1000 : -1000,
+            opacity: 0,
+        }
+    },
+    center: {
+        zIndex: 1,
+        x: '-50%',
+        y: '-50%',
+        opacity: 1,
+    },
+    exit: (direction: number) => {
+        return {
+            zIndex: 0,
+            y: '-50%',
+            x: direction < 0 ? 1000 : -1000,
+            opacity: 0,
+        }
+    },
+}
 
-    const goToSlide = (j: number) => {
-        if (j == slideDetails.length) {
-            setSlide(0)
-        } else if (j == -1) {
-            setSlide(slideDetails.length - 1)
+const Index = () => {
+    const router = useRouter()
+    const [[page, direction], setPage] = useState([0, 0])
+
+    const slidify = (newDirection: number) => {
+        if (page == slideDetails.length - 1 && newDirection == 1) {
+            setPage([0, newDirection])
+        } else if (page == 0 && newDirection < 0) {
+            setPage([slideDetails.length - 1, newDirection])
         } else {
-            setSlide(j)
+            setPage([page + newDirection, newDirection])
         }
     }
 
     return (
         <div
             sx={{
-                display: 'flex',
-                flexDirection: ['column', 'row'],
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                // p: [80, 0, 50, 200],
-                backgroundImage: (t) => `
-		            linear-gradient(
-                        to top,
-                        ${alpha('black', 0.0)(t)},
-                        ${alpha('black', 0.05)(t)}
-		            )
-		            `,
+                overflow: 'hidden',
+                bg: 'gray.3',
+                backgroundImage:
+                    'radial-gradient(circle, #d7d7dd, #e1e1e5, #ebebee, #f5f5f6, #ffffff)',
             }}
         >
+            <motion.div
+                sx={{
+                    transform: 'translate3d(0, 0, 0) scale(1, 1)',
+                    height: '100vh',
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                    const swipe = Math.abs(offset.x) * velocity.x
+                    if (swipe < -1000) {
+                        slidify(1)
+                    } else if (swipe > 1000) {
+                        slidify(-1)
+                    }
+                }}
+            >
+                <AnimatePresence initial={false} custom={direction}>
+                    <motion.div
+                        key={page}
+                        custom={direction}
+                        variants={slideVariants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        transition={{
+                            x: { type: 'spring', stiffness: 200, damping: 20 },
+                            opacity: { duration: 0.2 },
+                        }}
+                        sx={{
+                            position: 'absolute',
+                            left: '50%',
+                            top: '50%',
+                        }}
+                    >
+                        <ProjectCard
+                            title={slideDetails[page].title}
+                            image={slideDetails[page].image}
+                            description={slideDetails[page].description}
+                            tags={slideDetails[page].tags}
+                            onCreateClick={() =>
+                                router.push(slideDetails[page].path + '/create')
+                            }
+                            onExploreClick={() =>
+                                router.push(
+                                    slideDetails[page].path + '/explore'
+                                )
+                            }
+                        />
+                    </motion.div>
+                </AnimatePresence>
+            </motion.div>
             <IconButton
                 aria-label="Previous slide"
-                onClick={() => goToSlide(slide - 1)}
+                onClick={() => slidify(-1)}
                 sx={{
+                    position: 'absolute',
+                    top: [null, '50%'],
+                    left: ['7%', null, null, '20%', '30%'],
+                    bottom: ['-1%', null],
+                    transform: [null, 'translate(0, -50%)'],
+                    height: ['6rem', '8rem'],
                     cursor: 'pointer',
                     ':hover': {
                         opacity: '0.8',
@@ -97,22 +170,16 @@ const Index = () => {
                     <path d="M8.388,10.049l4.76-4.873c0.303-0.31,0.297-0.804-0.012-1.105c-0.309-0.304-0.803-0.293-1.105,0.012L6.726,9.516c-0.303,0.31-0.296,0.805,0.012,1.105l5.433,5.307c0.152,0.148,0.35,0.223,0.547,0.223c0.203,0,0.406-0.08,0.559-0.236c0.303-0.309,0.295-0.803-0.012-1.104L8.388,10.049z"></path>
                 </svg>
             </IconButton>
-            <ProjectCard
-                title={slideDetails[slide].title}
-                image={slideDetails[slide].image}
-                description={slideDetails[slide].description}
-                tags={slideDetails[slide].tags}
-                onCreateClick={() =>
-                    router.push(slideDetails[slide].path + '/create')
-                }
-                onExploreClick={() =>
-                    router.push(slideDetails[slide].path + '/explore')
-                }
-            />
             <IconButton
                 aria-label="Next slide"
-                onClick={() => goToSlide(slide + 1)}
+                onClick={() => slidify(1)}
                 sx={{
+                    position: 'absolute',
+                    top: [null, '50%'],
+                    right: ['7%', null, null, '20%', '30%'],
+                    bottom: ['-1%', null],
+                    transform: [null, 'translate(0, -50%)'],
+                    height: ['6rem', '8rem'],
                     cursor: 'pointer',
                     ':hover': {
                         opacity: '0.8',
