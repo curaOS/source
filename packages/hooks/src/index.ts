8064163c3eb51a590a04d15dc377384795eb2894
achraf elghinoussi <afaithraf@gmail.com>
2021-10-25 18:28:24 +0100

    new: useNFTContentType hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,4 +1,5 @@
 import useNFTContract, { useNFTMethod } from './useNFTContract'
+import { useNFTContentType } from './useNFTContent'
 import useMarketContract, { useMarketMethod } from './useMarketContract'
 import useFTContract, { useFTMethod } from './useFTContract'
 import { NearHooksProvider, useNearHooksContainer } from './near'
@@ -6,6 +7,7 @@ import { NearHooksProvider, useNearHooksContainer } from './near'
 export {
     useNFTContract,
     useNFTMethod,
+    useNFTContentType,
     useMarketContract,
     useMarketMethod,
     useFTContract,
