43dad396ce4faf3dc1b797af474f0bf2690ea5bc
achraf elghinoussi <afaithraf@gmail.com>
2021-10-25 19:35:08 +0100

    updated: components index

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,22 +2,24 @@ import { Bidders } from './Bidders'
 import { BidCreate } from './BidCreate'
 import { CreatorShare } from './CreatorShare'
 import { Metadata } from './Metadata'
-import { RenderIframe } from './RenderIframe'
 import { Footer } from './Footer'
 import { BiddersBids } from './BiddersBids'
 import { Header } from './Header'
 import { ProjectCard } from './ProjectCard'
 import { Menu } from './Menu'
+import { MediaObject } from './MediaObject'
+import { NFTE } from './NFTE'
 
 export {
     Bidders,
     BidCreate,
     CreatorShare,
     Metadata,
-    RenderIframe,
     BiddersBids,
     Footer,
     Header,
     Menu,
     ProjectCard,
+    MediaObject,
+    NFTE,
 }
