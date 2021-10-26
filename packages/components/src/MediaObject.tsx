1f77753430ea02ea559b5b59a1803f4005b6c1d0
achraf elghinoussi <afaithraf@gmail.com>
2021-10-26 08:52:48 +0100

    added height and width prop to MediaObject component

--- packages/components/src/MediaObject.tsx
+++ packages/components/src/MediaObject.tsx
@@ -1,9 +1,10 @@
 // @ts-nocheck
+/** @jsxImportSource theme-ui */
 
 import { useState, useEffect } from 'react'
 import { useNFTContentType } from '@cura/hooks'
 
-function Text({ media }: { media: string }) {
+function Text({ media, width, height }) {
     const [content, setContent] = useState('')
 
     useEffect(() => {
@@ -12,34 +13,47 @@ function Text({ media }: { media: string }) {
             .then((r) => setContent(r))
     }, [])
 
-    return <div>{content}</div>
+    return (
+        <div
+            sx={{
+                width: width,
+                height: height,
+            }}
+        >
+            {content}
+        </div>
+    )
 }
 
-function Video({ media, autoPlay }: { media: string; autoPlay: boolean }) {
+function Video({ media, width, height, autoPlay }) {
     return (
-        <video muted autoPlay={autoPlay} controls={!autoPlay} loop playsInline>
+        <video width={width} height={height} muted autoPlay={autoPlay} controls={!autoPlay} loop playsInline>
             <source src={media} />
         </video>
     )
 }
 
-function Audio({ media }: { media: string }) {
+function Audio({ media, width, height, }) {
     return <audio controls src={media}></audio>
 }
 
-function Image({ media }: { media: string }) {
-    return <img src={media} />
+function Image({ media, width, height, }) {
+    return <img width={width} height={height} src={media} />
 }
 
-function Iframe({ media }: { media: string }) {
-    return <iframe src={media} frameBorder="0" scrolling="no"></iframe>
+function Iframe({ media, width, height, }) {
+    return <iframe width={width} height={height} src={media} frameBorder="0" scrolling="no"></iframe>
 }
 
 export function MediaObject({
     mediaURI,
+    width,
+    height,
     autoPlay = false,
 }: {
     mediaURI?: string
+    width?: number
+    height?: number
     autoPlay: boolean
 }) {
     const [mediaType, setMediaType] = useState()
@@ -51,19 +65,50 @@ export function MediaObject({
 
     switch (mediaType) {
         case 'image':
-            return <Image media={mediaURI} />
+            return (
+                <Image
+                    media={mediaURI}
+                    width={width}
+                    height={height ? height : width}
+                />
+            )
 
         case 'video':
-            return <Video media={mediaURI} autoPlay={autoPlay} />
+            return (
+                <Video
+                    media={mediaURI}
+                    width={width}
+                    height={height ? height : width}
+                    autoPlay={autoPlay}
+                />
+            )
 
         case 'audio':
-            return <Audio media={mediaURI} />
+            return (
+                <Audio
+                    media={mediaURI}
+                    width={width}
+                    height={height ? height : width}
+                />
+            )
 
         case 'text':
-            return <Text media={mediaURI} />
+            return (
+                <Text
+                    media={mediaURI}
+                    width={width}
+                    height={height ? height : width}
+                />
+            )
 
         case 'html' || 'other':
-            return <Iframe media={mediaURI} />
+            return (
+                <Iframe
+                    media={mediaURI}
+                    width={width}
+                    height={height ? height : width}
+                />
+            )
 
         default:
             return <></>
