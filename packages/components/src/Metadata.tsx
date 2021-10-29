83915d0c919a11cca29b9dd350f649839c46db86
achraf elghinoussi <afaithraf@gmail.com>
2021-10-29 15:46:01 +0100

    NFTE component updates

--- packages/components/src/Metadata.tsx
+++ packages/components/src/Metadata.tsx
@@ -1,87 +1,86 @@
-// @ts-nocheck
 /** @jsxImportSource theme-ui */
 
-import { Box, Link } from 'theme-ui'
+import { Box, Flex } from 'theme-ui'
 import { Placeholder } from './Placeholder'
 
+type NFTMetadataType = {
+    creator_id?: string
+    owner_id?:
+        | string
+        | {
+              Account: string
+          }
+    metadata: {
+        title?: string
+        description?: string
+    }
+}
+
 export function Metadata({
-    title,
-    description,
-    creator,
-    owner,
-    loading = false,
-    width = 300,
+    data,
+    loading = true,
+    width = 400,
 }: {
-    title?: string
-    description?: string
-    creator?: string
-    owner?: string
-    loading?: boolean
-    width?: number
+    data: NFTMetadataType
+    loading: boolean
+    width: number | string
 }) {
     return (
         <Box
             sx={{
-                width: width + 'px',
+                width: width,
+                maxWidth: '90vw',
                 px: 20,
                 fontFamily: 'inherit',
             }}
         >
-            {loading ? (
-                <Placeholder width={width * 0.4} style={{mt: 3}}/>
-            ) : (
-                title && (
-                    <p
-                        sx={{
-                            mt: 2,
-                            mb: 1,
-                            fontWeight: 700,
-                            fontSize: 20,
-                        }}
-                    >
-                        {title}
-                    </p>
-                )
-            )}
-
-            {loading ? (
-                <Box mt={3}>
-                    <Placeholder width={width * 0.8} />
-                    <Placeholder width={width * 0.5} style={{mb: 0}} />
-                </Box>
-            ) : (
-                description && (
-                    <p
-                        sx={{
-                            fontSize: 16,
-                            opacity: 0.8,
-                            my: 0,
-                        }}
-                    >
-                        {description}
-                    </p>
-                )
-            )}
-
-            {(creator || loading) && (
-                <div
+            {loading ? <MetadataLoading /> : <MetadataLoaded {...data} />}
+        </Box>
+    )
+}
+function MetadataLoaded({
+    metadata: { title, description },
+    creator_id: creator,
+    owner_id: owner,
+}: NFTMetadataType) {
+    return (
+        <>
+            {title && (
+                <p
                     sx={{
-                        mt: 3,
-                        display: 'inline-block',
+                        mt: 2,
+                        mb: 1,
+                        fontWeight: 700,
+                        fontSize: 20,
                     }}
                 >
-                    <p
-                        sx={{
-                            fontSize: 14,
-                            fontWeight: 500,
-                            my: 0,
-                        }}
-                    >
-                        Created by
-                    </p>
-                    {loading ? (
-                        <Placeholder width={width * 0.3} />
-                    ) : (
+                    {title}
+                </p>
+            )}
+
+            {description && (
+                <p
+                    sx={{
+                        fontSize: 16,
+                        opacity: 0.8,
+                        my: 0,
+                    }}
+                >
+                    {description}
+                </p>
+            )}
+            <Flex sx={{ justifyContent: 'space-between', flexWrap: 'wrap' }}>
+                {creator && (
+                    <Box mt={3}>
+                        <p
+                            sx={{
+                                fontSize: 14,
+                                fontWeight: 500,
+                                my: 0,
+                            }}
+                        >
+                            Created by
+                        </p>
                         <a
                             href={`https://explorer.testnet.near.org/accounts/${creator}`}
                             target="_blank"
@@ -98,29 +97,20 @@ export function Metadata({
                         >
                             {creator}
                         </a>
-                    )}
-                </div>
-            )}
+                    </Box>
+                )}
 
-            {(owner || loading) && (
-                <div
-                    sx={{
-                        mt: 3,
-                        float: 'right',
-                    }}
-                >
-                    <p
-                        sx={{
-                            fontSize: 14,
-                            fontWeight: 500,
-                            my: 0,
-                        }}
-                    >
-                        Owned by
-                    </p>
-                    {loading ? (
-                        <Placeholder width={width * 0.3} />
-                    ) : (
+                {owner && (
+                    <Box mt={3}>
+                        <p
+                            sx={{
+                                fontSize: 14,
+                                fontWeight: 500,
+                                my: 0,
+                            }}
+                        >
+                            Owned by
+                        </p>
                         <a
                             href={`https://explorer.testnet.near.org/accounts/${owner}`}
                             target="_blank"
@@ -137,9 +127,47 @@ export function Metadata({
                         >
                             {owner}
                         </a>
-                    )}
-                </div>
-            )}
-        </Box>
+                    </Box>
+                )}
+            </Flex>
+        </>
+    )
+}
+function MetadataLoading() {
+    return (
+        <>
+            <Placeholder width="40%" style={{ mt: 3 }} />
+            <Box mt={3}>
+                <Placeholder width="70%" />
+                <Placeholder width="50%" style={{ mb: 0 }} />
+            </Box>
+            <Flex sx={{ justifyContent: 'space-between', flexWrap: 'wrap' }}>
+                <Box mt={3}>
+                    <p
+                        sx={{
+                            fontSize: 14,
+                            fontWeight: 500,
+                            my: 0,
+                        }}
+                    >
+                        Created by
+                    </p>
+                    <Placeholder width="120%" />
+                </Box>
+
+                <Box mt={3}>
+                    <p
+                        sx={{
+                            fontSize: 14,
+                            fontWeight: 500,
+                            my: 0,
+                        }}
+                    >
+                        Owned by
+                    </p>
+                    <Placeholder width="120%" />
+                </Box>
+            </Flex>
+        </>
     )
 }
