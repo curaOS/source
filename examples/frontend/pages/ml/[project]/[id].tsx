6ff40b2922a9a2caa20a161fdcdf0ba68db0e022
achraf elghinoussi <afaithraf@gmail.com>
2021-11-02 17:28:23 +0100

    Updated frontend to not use gas with nft_token method

--- examples/frontend/pages/ml/[project]/[id].tsx
+++ examples/frontend/pages/ml/[project]/[id].tsx
@@ -13,7 +13,6 @@ import { useNFTContract, useNFTMethod, useMarketMethod } from '@cura/hooks'
 import { accountState } from 'state/account'
 import { getFrameWidth } from 'utils/frame-width'
 import { useStatusUpdate } from 'utils/hooks-helpers'
-const CONTRACT_VIEW_GAS = utils.format.parseNearAmount('0.00000000010') // 100 Tgas
 const CONTRACT_BURN_GAS = utils.format.parseNearAmount('0.00000000029') // 290 Tgas
 const MARKET_ACCEPT_BID_GAS = utils.format.parseNearAmount('0.00000000025') // 250 Tgas
 const YOCTO_NEAR = utils.format.parseNearAmount('0.000000000000000000000001')
@@ -41,7 +40,7 @@ const MLProject = ({}) => {
             token_id: router.query.id,
             limit: 2,
         },
-        CONTRACT_VIEW_GAS,
+        undefined,
         updateStatus
     )
 
