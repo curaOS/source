1f034e96b23c080433834f9eca3de0841f584e7a
achraf elghinoussi <afaithraf@gmail.com>
2021-11-05 11:25:36 +0100

    added fonts variants to theme.ts

--- examples/frontend/theme.ts
+++ examples/frontend/theme.ts
@@ -1,11 +1,53 @@
 import { tailwind } from '@theme-ui/presets'
 
+// when updating components use text variants for typography (e.g. text.body, text.navigation...) and add more variants if there is any missing
 export const theme = {
     ...tailwind,
     fonts: {
-        ...tailwind.fonts,
-        body: 'IBM Plex Sans, sans-serif',
-        heading: 'IBM Plex Sans, sans-serif',
+        body: 'Acumin Pro, Helvetica, Arial, sans-serif',
+        heading: 'Acumin Pro, Helvetica, Arial, sans-serif',
+        monospace: 'Neue Machina, monospace',
+    },
+    fontSizes: [17, 19, 21, 26, 52],
+    fontWeights: {
+        body: 400,
+        normal: 400,
+        extrabold: 800,
+    },
+    lineHeights: {
+        body: 1,
+    },
+    text: {
+        body: {
+            fontFamily: 'body',
+            lineHeight: 'body',
+            fontWeight: 'body',
+            fontSize: 2,
+        },
+        navigation: {
+            fontFamily: 'monospace',
+            fontWeight: 'normal',
+            fontSize: 2,
+            textTransform: 'lowercase',
+        },
+        buttons: {
+            0: {
+                fontFamily: 'monospace',
+                fontWeight: 'normal',
+                fontSize: 0,
+            },
+            1: {
+                fontFamily: 'monospace',
+                fontWeight: 'extrabold',
+                fontSize: 2,
+                textTransform: 'uppercase',
+            },
+        },
+        currency: {
+            fontFamily: 'monospace',
+            fontWeight: 'extrabold',
+            fontSize: 3,
+        },
     },
     buttons: {
         uno: {
@@ -51,6 +93,14 @@ export const theme = {
                 bg: 'orange.5',
                 color: 'orange.2',
             },
+        },
+    },
+    styles: {
+        root: {
+            variant: 'text.body',
+        },
+        a: {
+            variant: 'text.navigation',
         },
     },
 }
