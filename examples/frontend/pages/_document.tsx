9959dfb0e5a5c3a135ed4235f3d9a555521f0b35
Achraf <54310457+achrafismyname@users.noreply.github.com>
2021-11-06 14:43:55 +0100

    Fonts update (#100)
    * Uploading new fonts
    
    * Removing old font from _document.tsx and importing the new to _app.tsx
    
    * added fonts variants to theme.ts

--- examples/frontend/pages/_document.tsx
+++ examples/frontend/pages/_document.tsx
@@ -9,12 +9,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head>
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
-                    />
-                </Head>
+                <Head></Head>
                 <body>
                     <Main />
                     <NextScript />
