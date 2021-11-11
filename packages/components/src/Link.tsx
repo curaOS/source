8cf83378cc40f55b85c4d756453e97f2013258e4
Leonardo <leonardo.placanica@gmail.com>
2021-11-11 15:30:24 +0100

    feat: updated Link component to use external module component via props and remove next lib

--- packages/components/src/Link.tsx
+++ packages/components/src/Link.tsx
@@ -1,16 +1,14 @@
-import type { ReactChild, ReactChildren } from 'react';
-import LinkNext from "next/link";
+import React, { DetailedReactHTMLElement, ReactChild, ReactChildren } from 'react';
 
-export const Link = ({ href, children }: { href: string, children?: ReactChild | ReactChildren }) => {
+export const Link = ({ href, children, LinkComponent, ...props }: { href: string, LinkComponent: DetailedReactHTMLElement<any, any>, children?: ReactChild | ReactChildren }) => {
   const isLocal = !(href.includes("http:") || href.includes("https:"));
+
   if (!isLocal) return (
-    <a href={href}>
+    <a href={href} {...props}>
       {children}
     </a>
   )
   else return (
-    <LinkNext href={href}>
-      {children}
-    </LinkNext>
+    React.cloneElement(LinkComponent, { href, children, ...props })
   );
 }
