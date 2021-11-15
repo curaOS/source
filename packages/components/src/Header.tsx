40b7b8cdfaccdc12b1810347205226cdc2372dfb
achraf elghinoussi <afaithraf@gmail.com>
2021-11-15 14:23:03 +0100

    Components: Header design update

--- packages/components/src/Header.tsx
+++ packages/components/src/Header.tsx
@@ -1,7 +1,8 @@
 // @ts-nocheck
 /** @jsxImportSource theme-ui */
 
-import { Text, Alert, Button, Close } from 'theme-ui'
+import { useState } from 'react'
+import { Text, Alert, Button, Close, Box, Container } from 'theme-ui'
 
 export const Header = ({
     base,
@@ -20,18 +21,131 @@ export const Header = ({
     setAlertMessage: () => void
     title: string
 }) => {
+    const [isButtonHovered, setIsButtonHovered] = useState(false)
+    const titleArray = title.split(/(?<=\/)/)
+
+    const UserButton = () => {
+        if (!accountId) {
+            return (
+                <Button onClick={onSignIn} variant="primary">
+                    Connect NEAR
+                </Button>
+            )
+        }
+
+        if (isButtonHovered) {
+            return (
+                <Button
+                    onClick={onSignOut}
+                    onMouseLeave={() => setIsButtonHovered(false)}
+                    variant="outline"
+                >
+                    Disconnect
+                </Button>
+            )
+        }
+
+        return (
+            <Button
+                onMouseEnter={() => setIsButtonHovered(true)}
+                variant="outline"
+            >
+                {accountId}
+            </Button>
+        )
+    }
     return (
-        <div>
-            <header
+        <Box
+            sx={{
+                width: '100%',
+                bg: 'bg',
+                borderBottom: accountId ? 0 : 1,
+            }}
+        >
+            <Container
+                as="header"
+                variant="wide"
+                pt={[2, 60]}
+                pb={[0, 36]}
+                px={0}
                 sx={{
-                    marginBottom: `1.45rem`,
-                    margin: `0 auto`,
-                    maxWidth: 960,
-                    padding: `1rem 2rem 0 2rem`,
+                    display: 'flex',
+                    flexDirection: ['column', 'row'],
+                    alignItems: ['left', 'center'],
+                    flexWrap: 'wrap',
                 }}
             >
+                <Box
+                    p={3}
+                    sx={{
+                        flex: 1,
+                        textAlign: 'left',
+                        borderBottom: [1, 0],
+                    }}
+                >
+                    <a
+                        href="/"
+                        sx={{
+                            variant: 'images.navLogo',
+                            display: 'block',
+                        }}
+                    ></a>
+                </Box>
+                <a
+                    href={`/${base}/bids`}
+                    sx={{
+                        p: 3,
+                        flex: 1,
+                        textAlign: ['left', 'center'],
+                        textDecoration: 'none',
+                        color: 'text',
+                    }}
+                >
+                    <Text
+                        sx={{
+                            display: ['block', 'none'],
+                        }}
+                    >
+                        {accountId ? accountId : 'blank'}
+                    </Text>
+                    <Text
+                        variant="title"
+                        sx={{
+                            fontWeight: 'normal',
+                            display: 'inline-block',
+                        }}
+                    >
+                        {titleArray[1] && titleArray[0]}
+                    </Text>
+                    <Text
+                        variant="title"
+                        sx={{
+                            display: 'inline-block',
+                        }}
+                    >
+                        {titleArray[1] ? titleArray[1] : titleArray[0]}
+                    </Text>
+                    <Text
+                        variant="body"
+                        m={[0, 2]}
+                        sx={{
+                            display: 'inline-block',
+                        }}
+                    >
+                        /PROJECTS
+                    </Text>
+                </a>
+                <Box
+                    sx={{
+                        flex: 1,
+                        textAlign: 'right',
+                        display: ['none', 'block'],
+                    }}
+                >
+                    <UserButton />
+                </Box>
                 {alertMessage && (
-                    <Alert>
+                    <Alert mt={2}>
                         <Text>{alertMessage.substring(0, 80)} ...</Text>
                         <Close
                             ml="auto"
@@ -41,83 +155,7 @@ export const Header = ({
                         />
                     </Alert>
                 )}
-                <div
-                    sx={{
-                        margin: `1rem auto 0rem auto`,
-                        background: `black`,
-                        maxWidth: 960,
-                        padding: `0.02rem 0rem`,
-                    }}
-                />
-                <div
-                    sx={{
-                        display: 'flex',
-                        flexDirection: ['column', 'row'],
-                        justifyContent: ['space-between'],
-                        alignItems: ['center'],
-                        mt: [3, 0],
-                        flexWrap: 'wrap',
-                    }}
-                >
-                    <div
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            mx: 3,
-                            flexWrap: 'wrap',
-                            flex: '1 1 0%',
-                        }}
-                    >
-                        <>
-                            {!accountId ? (
-                                <Button onClick={onSignIn} variant="due">
-                                    Connect NEAR
-                                </Button>
-                            ) : (
-                                <Button onClick={onSignOut} variant="uno">
-                                    Disconnect
-                                </Button>
-                            )}
-                        </>
-                    </div>
-                    <a href="/">
-                        <Text
-                            sx={{
-                                flex: 'none',
-                                color: 'black',
-                                textDecoration: 'none',
-                                fontFamily: 'IBM Plex Sans, sans-serif',
-                                fontWeight: 600,
-                                fontSize: '4rem',
-                                marginRight: '1rem',
-                            }}
-                        >
-                            {title}
-                        </Text>
-                    </a>
-                    <div
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            mx: 3,
-                            flexWrap: 'wrap',
-                            flex: '1 1 0%',
-                        }}
-                    >
-                        {accountId ? (
-                            <div>
-                                <a href={`/${base}/bids`}>{accountId}</a>
-                            </div>
-                        ) : (
-                            <div>[blank]</div>
-                        )}
-                    </div>
-                </div>
-            </header>
-        </div>
+            </Container>
+        </Box>
     )
 }
