a50ee6c722679ffa0d080ab4699a00b3e0b32637
Muazzam Kazmi <muazzam_ali@live.com>
2021-11-17 17:34:16 +0500

    Merge nft tests

--- packages/contracts/tests/nft.js
+++ packages/contracts/tests/nft.js
@@ -3,6 +3,10 @@ const BN = require('bn.js')
 const fs = require('fs').promises
 const assert = require('assert').strict
 
+const CONTRACT_CLAIM_GAS = nearAPI.utils.format.parseNearAmount('0.00000000029') // 300 Tgas
+const CONTRACT_CLAIM_PRICE = nearAPI.utils.format.parseNearAmount('1') // 1N
+const NUMBER_OF_NFT = 5
+
 function getConfig(env) {
     switch (env) {
         case 'sandbox':
@@ -11,6 +15,7 @@ function getConfig(env) {
                 networkId: 'sandbox',
                 nodeUrl: 'http://localhost:3030',
                 masterAccount: 'test.near',
+                marketAccount: 'market.test.near',
                 contractAccount: 'nft.test.near',
                 keyPath: '/tmp/near-sandbox/validator_key.json',
             }
@@ -18,8 +23,8 @@ function getConfig(env) {
 }
 
 const contractMethods = {
-    viewMethods: ['nft_metadata'],
-    changeMethods: ['init'],
+    viewMethods: ['nft_total_supply', 'nft_metadata', 'nft_token', 'nft_tokens_for_owner'],
+    changeMethods: ['init', 'claim_media'],
 }
 let config
 let masterAccount
@@ -62,7 +67,7 @@ async function createContractUser(
     await masterAccount.createAccount(
         accountId,
         pubKey,
-        new BN(10).pow(new BN(25))
+        new BN(10).pow(new BN(27))
     )
     keyStore.setKey(config.networkId, accountId, masterKey)
     const account = new nearAPI.Account(near.connection, accountId)
@@ -80,6 +85,14 @@ async function initTest() {
         config.contractAccount,
         pubKey,
         contract,
+        new BN(10).pow(new BN(27))
+    )
+
+    const contractM = await fs.readFile('./build/release/Market.wasm')
+    await masterAccount.createAndDeployContract(
+        config.marketAccount,
+        pubKey,
+        contractM,
         new BN(10).pow(new BN(25))
     )
 
@@ -88,6 +101,7 @@ async function initTest() {
         config.contractAccount,
         contractMethods
     )
+
 
     console.log('Finish deploy contracts and create test accounts')
     return { aliceUseContract }
@@ -104,14 +118,30 @@ async function test() {
                 name: 'nftExample',
                 symbol: 'NFTEXAMPLE',
             },
-            market_contract: 'market.testnet',
+            market_contract: 'market.test.near',
         },
     })
-
     const example_nft_metadata = await aliceUseContract.nft_metadata({
         args: {},
     })
 
+    // creating sample NFTs
+    for (var i=0; i<NUMBER_OF_NFT; i++)
+    {
+        console.log((i+1) + "/" + NUMBER_OF_NFT)
+        await aliceUseContract.claim_media( {args: {tokenMetadata: {media: ('NFT-' + i)}},
+  gas: CONTRACT_CLAIM_GAS, amount: CONTRACT_CLAIM_PRICE })
+    }
+
+    console.log('Finish create sample NFTs')
+
+    const total_supply = await aliceUseContract.nft_total_supply({
+        args: {},
+    })
+    const token_owners = await aliceUseContract.nft_tokens_for_owner({account_id: 'alice.test.near'})
+
+    assert.equal(parseInt(total_supply), NUMBER_OF_NFT)
+    assert.equal(token_owners.length, NUMBER_OF_NFT)
     assert.equal(example_nft_metadata.spec, 'nft-1.0.0')
     assert.equal(example_nft_metadata.name, 'nftExample')
     assert.equal(example_nft_metadata.symbol, 'NFTEXAMPLE')
