fabc92dbf56556b0b7922179c0ff8fea447975a1
Achraf <54310457+achrafismyname@users.noreply.github.com>
2021-11-20 11:13:06 +0100

    Project Gallery with paging (#130)
    * Frontend: Updated "Share" explore page to a new gallery
    
    * Frontend: Updated CC/ explore page to a new gallery
    
    * Frontend: Updated ML/ explore page to a new gallery
    
    * Replaced infinite scroll logic with 'react-infinite-scroll-component'
    
    closes #88

---
+++ examples/frontend/pages/share/explore/[id].tsx
@@ -0,0 +1,115 @@
+// @ts-nocheck
+/** @jsxImportSource theme-ui */
+
+import { Box, Divider } from 'theme-ui'
+import { utils } from 'near-api-js'
+import { useRouter } from 'next/router'
+import Layout from '../../../containers/Layout'
+import {
+    CreatorShare,
+    Metadata,
+    MediaObject,
+    BidCreate,
+} from '@cura/components'
+import { alertMessageState, indexLoaderState } from '../../../state/recoil'
+import { useSetRecoilState } from 'recoil'
+import {
+    useNFTContract,
+    useNFTMethod,
+    useNearHooksContainer,
+} from '@cura/hooks'
+import { getFrameWidth } from 'utils/frame-width'
+import { useStatusUpdate } from 'utils/hooks-helpers'
+import { mapPathToProject } from 'utils/path-to-project'
+
+const MARKET_SET_BID_GAS = utils.format.parseNearAmount('0.00000000020') // 200 Tgas
+
+const HARDCODED_ROYALTY_ADDRESS = process.env.YSN_ADDRESS
+const HARDCODED_ROYALTY_SHARE = '2500'
+
+const ExploreToken = () => {
+    const router = useRouter()
+    const { updateStatus } = useStatusUpdate()
+    const setAlertMessage = useSetRecoilState(alertMessageState)
+    const setIndexLoader = useSetRecoilState(indexLoaderState)
+
+    const contractAdress = router && mapPathToProject(router.asPath)
+    const project = 'share'
+
+    const { contract } = useNFTContract(contractAdress)
+    const { accountId } = useNearHooksContainer()
+
+    const { data: media } = useNFTMethod(
+        contractAdress,
+        'nft_token',
+        {
+            token_id: router.query.id,
+        },
+        undefined,
+        updateStatus
+    )
+
+    async function setBid(amount, resale) {
+        setIndexLoader(true)
+        try {
+            await contract.set_bid(
+                {
+                    token_id: media.id,
+                    amount: utils.format.parseNearAmount(amount),
+                    bidder: accountId,
+                    recipient: media.owner_id,
+                    sell_on_share: parseInt(resale) * 100,
+                    currency: 'near',
+                },
+                MARKET_SET_BID_GAS,
+                utils.format.parseNearAmount(amount)
+            )
+        } catch (e) {
+            console.error(e)
+            setIndexLoader(false)
+            setAlertMessage(e.toString())
+        }
+    }
+
+    const designDimension = getFrameWidth()
+    return (
+        <Layout project={project}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: 12,
+                }}
+            >
+                {media && (
+                    <MediaObject
+                        mediaURI={`https://arweave.net/${media?.metadata?.media}`}
+                        width={designDimension}
+                        height={designDimension}
+                    />
+                )}
+                {media && (
+                    <Metadata
+                        data={media}
+                        loading={false}
+                        width={designDimension}
+                    />
+                )}
+                <Divider sx={{ width: 300 }} />
+                <CreatorShare
+                    address={HARDCODED_ROYALTY_ADDRESS}
+                    share={HARDCODED_ROYALTY_SHARE}
+                />
+                <Divider sx={{ width: 300 }} />
+                <BidCreate
+                    title={media?.metadata.title}
+                    creator={media?.owner_id}
+                    onBid={setBid}
+                />
+            </Box>
+        </Layout>
+    )
+}
+
+export default ExploreToken
---
+++ examples/frontend/pages/share/explore/index.tsx
@@ -0,0 +1,140 @@
+// @ts-nocheck
+/** @jsxImportSource theme-ui */
+
+import { useState, useEffect } from 'react'
+import { useSetRecoilState } from 'recoil'
+import { useRouter } from 'next/router'
+import { utils } from 'near-api-js'
+import { Box, Link, Spinner } from 'theme-ui'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+import { useNFTMethod, useNFTContract } from '@cura/hooks'
+import { MediaObject } from '@cura/components'
+
+import Layout from '../../../containers/Layout'
+import { getFrameWidth } from 'utils/frame-width'
+import { mapPathToProject } from 'utils/path-to-project'
+import { alertMessageState } from '../../../state/recoil'
+
+const CONTRACT_RANDOM_GAS = utils.format.parseNearAmount('0.00000000020') // 200 Tgas
+
+const NFT_PER_PAGE = 4
+
+const Explore = () => {
+    const router = useRouter()
+    const setAlertMessage = useSetRecoilState(alertMessageState)
+
+    const contractAdress = router && mapPathToProject(router.asPath)
+    const project = 'share'
+
+    // get total supply, which we need to detect if all NFTs are loaded
+    const { data: totalSupply } = useNFTMethod(
+        contractAdress,
+        'nft_total_supply',
+        {}
+    )
+
+    // hook that contains all the logic of explore page
+    const { items, nextPage } = useNFTExplore(contractAdress, NFT_PER_PAGE)
+
+    function loadMore() {
+        try {
+            nextPage()
+        } catch (error) {
+            console.error(error)
+            setAlertMessage(error.toString())
+        }
+    }
+
+    const designDimension = getFrameWidth(true)
+
+    return (
+        <Layout project={project}>
+            <Box sx={{ textAlign: 'center' }}>
+                <InfiniteScroll
+                    dataLength={items.length}
+                    next={loadMore}
+                    hasMore={totalSupply > items.length}
+                    loader={
+                        <Box width="100%" mt={2}>
+                            <Spinner />
+                        </Box>
+                    }
+                >
+                    {items.map((item, index) => {
+                        return (
+                            <Link
+                                key={index}
+                                href={`/${project}/explore/${item.id}`}
+                                m={[1, 2, 3]}
+                                sx={{
+                                    display: 'inline-flex',
+                                    position: 'relative',
+                                    ':hover': {
+                                        opacity: '0.8',
+                                    },
+                                }}
+                            >
+                                <div
+                                    sx={{
+                                        position: 'absolute',
+                                        height: '100%',
+                                        width: '100%',
+                                        zIndex: 1,
+                                    }}
+                                ></div>
+                                <MediaObject
+                                    mediaURI={`https://arweave.net/${item.metadata.media}`}
+                                    width={designDimension}
+                                    height={designDimension}
+                                />
+                            </Link>
+                        )
+                    })}
+                </InfiniteScroll>
+            </Box>
+        </Layout>
+    )
+}
+
+export default Explore
+
+function useNFTExplore(contractAdress: string, limitPerPage = 4) {
+    const [currentPage, setCurrentPage] = useState(0)
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const from_index = Math.floor(limitPerPage * currentPage)
+
+    const { contract } = useNFTContract(contractAdress)
+
+    async function getData() {
+        const newData = await contract.nft_tokens(
+            {
+                from_index: from_index ? from_index.toString() : '0', // prevent toString() from converting 0 to NaN
+                limit: limitPerPage,
+            },
+            CONTRACT_RANDOM_GAS
+        )
+        setData([...data, ...newData])
+        setIsLoading(false)
+    }
+
+    // Excute function if contract change or currentPage change
+    useEffect(() => {
+        // make sure the user is logged in and the contract is the right contract
+        if (
+            contract?.account?.accountId &&
+            contract.contractId == contractAdress
+        ) {
+            setIsLoading(true)
+            getData()
+        }
+    }, [contract, currentPage])
+
+    return {
+        items: data,
+        loading: isLoading,
+        nextPage: () => setCurrentPage(currentPage + 1),
+    }
+}
