740f45dc863dfa7cee0c1868063335a0e2008f10
Yassine <yassinesania@gmail.com>
2021-11-23 11:39:16 +0100

    Contracts | Lint

--- packages/contracts/as/NFT/assembly/asserts.ts
+++ packages/contracts/as/NFT/assembly/asserts.ts
@@ -5,9 +5,15 @@ export function assert_deposit_attached(amount: u128): void {
 }
 
 export function assert_one_yocto(): void {
-    assert(u128.eq(context.attachedDeposit, u128.from('1')), 'Deposit is one yoctoNEAR.')
+    assert(
+        u128.eq(context.attachedDeposit, u128.from('1')),
+        'Deposit is one yoctoNEAR.'
+    )
 }
 
 export function assert_at_least_one_yocto(): void {
-    assert(u128.ge(context.attachedDeposit, u128.from('1')), 'Deposit is at least one yoctoNEAR.')
+    assert(
+        u128.ge(context.attachedDeposit, u128.from('1')),
+        'Deposit is at least one yoctoNEAR.'
+    )
 }
