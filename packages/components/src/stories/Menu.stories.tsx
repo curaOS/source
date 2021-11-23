4b843f95dacb3a1f7eaddbe1cd95b06d4ddcd808
achraf elghinoussi <afaithraf@gmail.com>
2021-11-23 17:23:45 +0100

    Updated stories to show connected/disconnected

--- packages/components/src/stories/Menu.stories.tsx
+++ packages/components/src/stories/Menu.stories.tsx
@@ -4,12 +4,26 @@ import { Menu } from '../Menu'
 export default {
     title: 'Components/Menu',
     component: Menu,
+    parameters: {
+        layout: 'fullscreen',
+    },
 } as ComponentMeta<typeof Menu>
 
-const Template: ComponentStory<typeof Menu> = (args) => <Menu {...args} />
+const Template: ComponentStory<typeof Menu> = (args) => {
+    return (
+        <div style={{ height: '100vh' }}>
+            <Menu {...args} />
+        </div>
+    )
+}
 
-export const Zero = Template.bind({})
-Zero.args = {
+export const Connected = Template.bind({})
+Connected.args = {
     base: 'cc',
     accountId: 'ys24.testnet',
+}
+
+export const Disconnected = Template.bind({})
+Disconnected.args = {
+    base: 'cc',
 }
