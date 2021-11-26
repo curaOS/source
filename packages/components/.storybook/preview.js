8d06a2fbc4cf1ce4cb600073e3d93be6356cd894
achraf elghinoussi <afaithraf@gmail.com>
2021-11-26 17:47:52 +0100

    Renamed `assets/fonts/style.css` to `assets/fonts/index.css`

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '../src/theme'
-import '../src/assets/fonts/style.css'
+import '../src/assets/fonts/index.css'
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
