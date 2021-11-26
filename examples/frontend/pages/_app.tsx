9d698bd83ddb27d1d2a92edd1a148a46f17157b9
achraf elghinoussi <afaithraf@gmail.com>
2021-11-26 18:13:55 +0100

    Import `fonts` from `@cura/components` to `frontend`

--- examples/frontend/pages/_app.tsx
+++ examples/frontend/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'theme-ui'
 import { theme } from '../theme'
 import { RecoilRoot } from 'recoil'
 import { NearHooksProvider } from '@cura/hooks'
-import '../public/fonts/style.css'
+import '@cura/components/dist/assets/fonts/index.css'
 
 function App({ Component, pageProps }: AppProps) {
     return (
