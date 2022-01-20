1cda484b870ecc9a143ce69868075ba0c21f3613
achraf elghinoussi <afaithraf@gmail.com>
2022-01-20 02:01:44 +0100

    add apollo provider

--- examples/frontend/pages/_app.tsx
+++ examples/frontend/pages/_app.tsx
@@ -6,14 +6,27 @@ import { RecoilRoot } from 'recoil'
 import { NearHooksProvider } from '@cura/hooks'
 import '@cura/components/assets/fonts/index.css'
 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: 'https://api.thegraph.com/subgraphs/name/achrafismyname/cura-subgraph',
+    cache: new InMemoryCache(),
+})
+
 function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
-            <RecoilRoot>
-                <NearHooksProvider>
-                    <Component {...pageProps} />
-                </NearHooksProvider>
-            </RecoilRoot>
+            <ApolloProvider client={client}>
+                <RecoilRoot>
+                    <NearHooksProvider>
+                        <Component {...pageProps} />
+                    </NearHooksProvider>
+                </RecoilRoot>
+            </ApolloProvider>
         </ThemeProvider>
     )
 }
